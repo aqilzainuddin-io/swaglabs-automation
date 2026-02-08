fb92d9a9a2340c57df944d49b8850eea1dbdf5f9
aqilzainuddin-writer <aqilzainuddin.io@gmail.com>
2026-02-09 06:13:46 +0800

    feat: add loginpage.java file

---
+++ src/main/java/pages/login/LoginPage.java
@@ -0,0 +1,36 @@
+package pages.login;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import pages.base.BasePage;
+
+public class LoginPage extends BasePage {
+
+    private By usernameField = By.id("user-name");
+    private By passwordField = By.id("password");
+    private By loginButton = By.id("login-button");
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void enterUsername(String username) {
+        type(usernameField, username);
+    }
+
+    public void enterPassword(String password) {
+        type(passwordField, password);
+    }
+
+    public void clickLogin() {
+        click(loginButton);
+    }
+
+    public void login(String username, String password) {
+        enterUsername(username);
+        enterPassword(password);
+        clickLogin();
+    }
+    
+}
