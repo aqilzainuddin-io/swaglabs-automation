7e23882aa3266107eba49dc234645ffc3ed46a9f
aqilzainuddin-writer <aqilzainuddin.io@gmail.com>
2026-02-09 11:42:53 +0800

    feat: disable password leak detection popout, driverfactory.java file

--- src/main/java/setup/DriverFactory.java
+++ src/main/java/setup/DriverFactory.java
@@ -1,6 +1,8 @@
 package setup;
 
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -15,6 +17,12 @@ public class DriverFactory {
         if (driver == null) {
 
             ChromeOptions options = new ChromeOptions();
+
+            // Set Chrome preferences to disable password manager popups
+            Map<String, Object> prefs = new HashMap<>();
+            prefs.put("profile.password_manager_leak_detection", false); 
+            prefs.put("credentials_enable_service", false); 
+            options.setExperimentalOption("prefs", prefs);
 
             if ("true".equalsIgnoreCase(EnvReader.get("HEADLESS"))) {
                 options.addArguments("--headless=new");
